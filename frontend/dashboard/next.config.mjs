a1094a2e81995f64de69281a2530fe7df1ea753b
axolmain <dun21010@byui.edu>
2024-10-19 03:26:47 -0600

    pwa friendly

--- frontend/dashboard/next.config.mjs
+++ frontend/dashboard/next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import runtimeCaching from 'next-pwa/cache.js';
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
+    // disable: process.env.NODE_ENV === "development",
+});
+
+const nextConfig = pwaConfig({
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
+});
 
 export default nextConfig;
