284b94847da6e38bdd5161c18a672d5d1a491dda
axolmain <dun21010@byui.edu>
2024-10-19 02:43:35 -0600

    updated manifest

--- frontend/in-egrity-funds/next.config.mjs
+++ frontend/in-egrity-funds/next.config.mjs
@@ -14,7 +14,6 @@ export default withPWA({
     dest: 'public',         // Destination for the PWA files
     register: true,         // Auto-register the service worker
     skipWaiting: true,      // Skip waiting phase for the SW
-    runtimeCaching,         // Runtime caching strategy
     buildExcludes: [/middleware-manifest\.json$/], // Exclude middleware manifest from cache
     maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
     disable: false,    // You can also add an "exclude" option here to exclude specific files from caching if needed
